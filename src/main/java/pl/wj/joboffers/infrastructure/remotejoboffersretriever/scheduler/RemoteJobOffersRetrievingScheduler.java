17a9865dd589e9481a0f6521de991671498b45b3
jankowskiw <waldemar.jankowski95@gmail.com>
2023-03-23 20:49:35 +0100

    Add @Component annotation for RemoteJobOffersRetrievingScheduler class and delay property in app.prop

--- src/main/java/pl/wj/joboffers/infrastructure/remotejoboffersretriever/scheduler/RemoteJobOffersRetrievingScheduler.java
+++ src/main/java/pl/wj/joboffers/infrastructure/remotejoboffersretriever/scheduler/RemoteJobOffersRetrievingScheduler.java
@@ -3,16 +3,18 @@ package pl.wj.joboffers.infrastructure.remotejoboffersretriever.scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import pl.wj.joboffers.domain.remotejobofferretriever.RemoteJobOfferRetrieverFacade;
 
 @Log4j2
+@Component
 @RequiredArgsConstructor
 public class RemoteJobOffersRetrievingScheduler {
     private final RemoteJobOfferRetrieverFacade remoteJobOfferRetrieverFacade;
 
-    @Scheduled(cron = "0 0 */3 * * *")
+    @Scheduled(fixedDelayString = "${job-offers.http.client.config.scheduler.delay}")
     public void retrieveRemoteJobOfferDtos() {
-        log.info("Retrieved remote job offers");
+        log.warn("Retrieved remote job offers");
         remoteJobOfferRetrieverFacade.retrieveRemoteJobOfferDtos();
     }
 }
