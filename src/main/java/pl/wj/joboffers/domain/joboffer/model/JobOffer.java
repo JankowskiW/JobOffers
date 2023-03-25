cf37e71a5d6da7dd9ded54bb30e2717b495231ec
jankowskiw <waldemar.jankowski95@gmail.com>
2023-03-25 10:25:44 +0100

    Add @Indexed annotation to set offerUrl as unique.

--- src/main/java/pl/wj/joboffers/domain/joboffer/model/JobOffer.java
+++ src/main/java/pl/wj/joboffers/domain/joboffer/model/JobOffer.java
@@ -2,6 +2,7 @@ package pl.wj.joboffers.domain.joboffer.model;
 
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
@@ -12,4 +13,7 @@ public record JobOffer (
         String title,
         String company,
         String salary,
-        String offerUrl){}
+        @Indexed(unique = true)
+        String offerUrl) {
+
+}
