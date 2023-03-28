eb68b506107106ed6581af395758fd5e7c45a0f6
jankowskiw <waldemar.jankowski95@gmail.com>
2023-03-28 21:51:36 +0200

    refactor validationMessages.properties

--- src/main/java/pl/wj/joboffers/domain/joboffer/model/dto/JobOfferRequestDto.java
+++ src/main/java/pl/wj/joboffers/domain/joboffer/model/dto/JobOfferRequestDto.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record JobOfferRequestDto(
-        @NotBlank(message = "{validation.request.job-offer.title.not-blank}")
+        @NotBlank(message = "{job-offer.title.not-blank}")
         String title,
-        @NotBlank(message = "{validation.request.job-offer.company.not-blank}")
+        @NotBlank(message = "{job-offer.company.not-blank}")
         String company,
-        @NotNull(message = "{validation.request.job-offer.salary.not-null}")
+        @NotNull(message = "{job-offer.salary.not-null}")
         String salary,
-        @NotBlank(message = "{validation.request.job-offer.offer-url.not-blank}")
+        @NotBlank(message = "{job-offer.offer-url.not-blank}")
         String offerUrl) {}
