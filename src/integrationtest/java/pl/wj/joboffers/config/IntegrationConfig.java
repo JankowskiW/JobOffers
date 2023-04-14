9722aa90d4d55e38bdf7c9cd6dcf6b163dd56d22
jankowskiw <waldemar.jankowski95@gmail.com>
2023-04-14 18:07:20 +0200

    Add redis dependencies and change integration clock bean type from Clock to AdjustableClock.

--- src/integrationtest/java/pl/wj/joboffers/config/IntegrationConfig.java
+++ src/integrationtest/java/pl/wj/joboffers/config/IntegrationConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import pl.wj.joboffers.util.AdjustableClock;
 
-import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 
 @Configuration
 @Profile("integration")
@@ -13,8 +16,11 @@ public class IntegrationConfig {
 
     @Bean
     @Primary
-    Clock clock() { // TODO: Change Clock to AdjustableClock
-        return Clock.systemUTC();
+    AdjustableClock clock() { // TODO: Change Clock to AdjustableClock
+        LocalDate localDate = LocalDate.of(2023, 3, 1);
+        LocalTime localTime = LocalTime.of(12,0);
+        ZoneId zoneId = ZoneId.systemDefault();
+        return AdjustableClock.ofLocalDateAndLocalTime(localDate, localTime, zoneId);
     }
 
 }
--- src/integrationtest/java/pl/wj/joboffers/remotejoboffersretriever/scheduler/RemoteJobOffersHttpRetrievingSchedulerIntegrationTest.java
+++ src/integrationtest/java/pl/wj/joboffers/remotejoboffersretriever/scheduler/RemoteJobOffersHttpRetrievingSchedulerIntegrationTest.java
@@ -1,20 +1,15 @@
 package pl.wj.joboffers.remotejoboffersretriever.scheduler;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.http.HttpStatus;
 import pl.wj.joboffers.BaseIntegrationTest;
 import pl.wj.joboffers.JobOffersApplication;
-import pl.wj.joboffers.domain.joboffer.JobOfferFacade;
-import pl.wj.joboffers.remotejoboffersretriever.RemoteJobOffersRetrieverIntegrationTestHelper;
 import pl.wj.joboffers.infrastructure.remotejoboffersretriever.http.RemoteJobOffersRetriever;
-import pl.wj.joboffers.domain.remotejoboffersretriever.dto.RemoteJobOfferDto;
+import pl.wj.joboffers.remotejoboffersretriever.RemoteJobOffersRetrieverIntegrationTestHelper;
 
 import java.time.Duration;
-import java.util.Set;
 
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.times;
@@ -24,52 +19,14 @@ import static org.mockito.Mockito.verify;
 public class RemoteJobOffersHttpRetrievingSchedulerIntegrationTest extends BaseIntegrationTest {
     @SpyBean
     private RemoteJobOffersRetriever remoteJobOffersRetriever;
-    @Autowired
-    private RemoteJobOffersRetrieverIntegrationTestHelper helper;
-    @Autowired
-    private JobOfferFacade jobOfferFacade;
 
     @Test
     void shouldInvokeRetrievingEverySpecifiedTime() {
         // given
-        int invocationsNumber = 2;
+        int invocationsNumber = 1;
         // when
         await().atMost(Duration.ofSeconds(2))
                 .untilAsserted(() -> verify(remoteJobOffersRetriever, times(invocationsNumber))
                         .retrieveRemoteJobOfferDtos());
-
-
-//        await().atMost(Duration.ofSeconds(20))
-//                .pollInterval(Duration.ofSeconds(1))
-//                .until( () -> {try { return true; } catch(RuntimeException e) { return false; }});
-
-    }
-
-
-
-    @Test
-    void shouldSaveJobOffersToDatabase() {
-        // given
-        String url = "/offers";
-        Set<RemoteJobOfferDto> remoteJobOfferDtos = helper.getRemoteJobOfferDtos();
-        wireMockServer.stubFor(WireMock.get(url)
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(helper.createBodyWithSomeJobOffers())));
-
-        int invocationsNumber = 2;
-        // when
-//        await().atMost(Duration.ofSeconds(2))
-//                .untilAsserted(() -> verify(remoteJobOffersRetriever, times(invocationsNumber))
-//                        .retrieveRemoteJobOfferDtos());
-
-        // when
-//        await()
-//                .atMost(Duration.ofSeconds(20))
-//                .pollInterval(Duration.ofSeconds(1))
-//                .until(() -> jobOfferFacade.getAllJobOffers().size() == remoteJobOfferDtos.size());
-
-        // then
     }
 }
